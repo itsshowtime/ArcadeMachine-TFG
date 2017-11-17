29b0ce9d2a4ca02644e0c2b87855b96a1dcf9fad
itsshowtime <kilian.pe.co@gmail.com>
2017-11-17 20:00:37 +0100

    works on emulationstation, doesn't work on retroarch

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ArcadeMachine-TFG
 My TFG project, gathering input from buttons and joystick using PIC, Raspberry Pi and RetroPie.
+
+check:
+  retrogame
+  retropie-GPIO modules
+  PetRockBlog & DigitalLumberjack approach
--- uinput.c
+++ uinput.c
@@ -2,34 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
 #include <fcntl.h>
 #include <errno.h>
+
 #include <linux/input.h>
 #include <linux/uinput.h>
 
+#include <bcm2835.h>
+
+#define PIN RPI_V2_GPIO_P1_29
 #define die(str, args...) do { \
         perror(str); \
         exit(EXIT_FAILURE); \
     } while(0)
 
-int
-main(void)
-{
+int main(void) {
+    if (!bcm2835_init())
+        return 1;
+
     int                    fd;
     struct uinput_user_dev uidev;
     struct input_event     ev;
-    int                    dx, dy;
-    int                    i;
+
+    bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_INPT);
+    bcm2835_gpio_set_pud(PIN, BCM2835_GPIO_PUD_UP);
 
     fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK | O_APPEND);
     if(fd < 0)
         die("error: open");
-
     if(ioctl(fd, UI_SET_EVBIT, EV_KEY) < 0)
         die("error: ioctl");
     if(ioctl(fd, UI_SET_KEYBIT, KEY_K) < 0)
         die("error: ioctl");
-
 
     memset(&uidev, 0, sizeof(uidev));
     snprintf(uidev.name, UINPUT_MAX_NAME_SIZE, "k stroker");
@@ -49,21 +54,22 @@ main(void)
     srand(time(NULL));
 
     while(1) {
-        switch(rand() % 4) {
-        case 0:
-            dx = 1;
-            break;
-        case 1:
-            dx = 1;
-            break;
-        case 2:
-            dx = 1;
-            break;
-        case 3:
-            dx = 1;
-            break;
-        }
+        uint8_t dx = bcm2835_gpio_lev(PIN);
+        memset(&ev, 0, sizeof(struct input_event));
+        ev.type = EV_KEY;
+        ev.code = KEY_K;
+        ev.value = dx;
+        if(write(fd, &ev, sizeof(struct input_event)) < 0)
+            die("error: write");
 
+        memset(&ev, 0, sizeof(struct input_event));
+        ev.type = EV_SYN;
+        ev.code = 0;
+        ev.value = 0;
+        if(write(fd,&ev,sizeof(struct input_event)) < 0)
+            die("error: write");
+        delay(20);
+/*
         for(i = 0; i < 100; i++) {
             memset(&ev, 0, sizeof(struct input_event));
             ev.type = EV_KEY;
@@ -91,7 +97,7 @@ main(void)
         }
 
         sleep(5);
-    }
+*/    }
 
     sleep(2);
 
