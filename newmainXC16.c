0db657f628e0c9cf129d502d8783e61c164615bc
Kilian Peiró <kilian.pe.co@gmail.com>
2018-02-19 11:53:57 +0100

    14 buttons now working

--- newmainXC16.c
+++ newmainXC16.c
@@ -4,6 +4,48 @@
  *
  * Created on 22 January 2018, 16:41
  */
+// FBS
+#pragma config BWRP = OFF               // Table Write Protect Boot (Boot segment may be written)
+#pragma config BSS = OFF                // Boot segment Protect (No boot program Flash segment)
+
+// FGS
+#pragma config GWRP = OFF               // General Segment Code Flash Write Protection bit (General segment may be written)
+#pragma config GCP = OFF                // General Segment Code Flash Code Protection bit (No protection)
+
+// FOSCSEL
+#pragma config FNOSC = FRCDIV           // Oscillator Select (8 MHz FRC oscillator with divide-by-N (FRCDIV))
+#pragma config IESO = OFF               // Internal External Switch Over bit (Internal External Switchover mode disabled (Two-Speed Start-up disabled))
+
+// FOSC
+#pragma config POSCMOD = NONE           // Primary Oscillator Configuration bits (Primary oscillator disabled)
+#pragma config OSCIOFNC = ON            // CLKO Enable Configuration bit (CLKO output disabled; pin functions as port I/O)
+#pragma config POSCFREQ = HS            // Primary Oscillator Frequency Range Configuration bits (Primary oscillator/external clock input frequency greater than 8 MHz)
+#pragma config SOSCSEL = SOSCHP         // SOSC Power Selection Configuration bits (Secondary oscillator configured for high-power operation)
+#pragma config FCKSM = CSDCMD           // Clock Switching and Monitor Selection (Both Clock Switching and Fail-safe Clock Monitor are disabled)
+
+// FWDT
+#pragma config WDTPS = PS32768          // Watchdog Timer Postscale Select bits (1:32,768)
+#pragma config FWPSA = PR128            // WDT Prescaler (WDT prescaler ratio of 1:128)
+#pragma config WINDIS = OFF             // Windowed Watchdog Timer Disable bit (Standard WDT selected; windowed WDT disabled)
+#pragma config FWDTEN = OFF             // Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
+
+// FPOR
+#pragma config BOREN = BOR0             // Brown-out Reset Enable bits (Brown-out Reset disabled in hardware; SBOREN bit disabled)
+#pragma config PWRTEN = OFF             // Power-up Timer Enable bit (PWRT disabled)
+#pragma config I2C1SEL = PRI            // Alternate I2C1 Pin Mapping bit (Default location for SCL1/SDA1 pins)
+#pragma config BORV = V18               // Brown-out Reset Voltage bits (Brown-out Reset set to lowest voltage (1.8V))
+#pragma config MCLRE = ON               // MCLR Pin Enable bit (MCLR pin enabled; RA5 input pin disabled)
+
+// FICD
+#pragma config ICS = PGx1               // ICD Pin Placement Select bits (PGC1/PGD1 are used for programming and debugging the device)
+
+// FDS
+#pragma config DSWDTPS = DSWDTPSF       // Deep Sleep Watchdog Timer Postscale Select bits (1:2,147,483,648 (25.7 Days))
+#pragma config DSWDTOSC = LPRC          // DSWDT Reference Clock Select bit (DSWDT uses LPRC as reference clock)
+#pragma config RTCOSC = SOSC            // RTCC Reference Clock Select bit (RTCC uses SOSC as reference clock)
+#pragma config DSBOREN = OFF            // Deep Sleep Zero-Power BOR Enable bit (Deep Sleep BOR disabled in Deep Sleep)
+#pragma config DSWDTEN = OFF            // Deep Sleep Watchdog Timer Enable bit (DSWDT disabled)
+
 #define USE_AND_OR
 
 #include "xc.h"
@@ -94,10 +136,29 @@ int main (void) {
     read_buffer[BUFFSIZE - 1]  = -1;
     write_buffer[BUFFSIZE - 1] = -1;
     
-    FOSCbits.POSCMD1  = 0;
-    FOSCbits.POSCMD0  = 0;
-    FOSCbits.OSCIOFNC = 0;
-    FICDbits.DEBUG    = 1; 
+    //disable modules to make buttons work
+    AD1PCFG = 0b1101110000111111;
+    PMD1    = 0b0011100001101001;
+    PMD2    = 0b0000000000000001;
+    PMD3    = 0b0000011010000000;
+    PMD4    = 0b0000000000001110;
+    IEC0    = 0x0000;
+    IEC1    = 0x0000;
+    IEC3    = 0x0000;
+    IEC4    = 0x0000;
+    CTMUCONbits.CTMUEN  = 0;
+    OSCCONbits.SOSCEN   = 0;
+    REFOCONbits.ROEN    = 0;
+    RCFGCALbits.RTCEN   = 0;
+    RCFGCALbits.RTCOE   = 0;
+    SPI1STATbits.SPIEN  = 0;
+    SPI1CON1bits.DISSCK = 1;
+    SPI1CON1bits.DISSDO = 1;
+    OC1CONbits.OCM2     = 0;
+    OC1CONbits.OCM1     = 0;
+    OC1CONbits.OCM0     = 0;
+    HLVDCONbits.HLVDEN  = 0;
+    CTMUCONbits.CTMUEN  = 0;
     
     I2C1CONbits.I2CEN   = 1;
     I2C1CONbits.I2CSIDL = 0;    
@@ -117,9 +178,6 @@ int main (void) {
     IPC4bits.SI2C1P0 = 1;
     IEC1bits.SI2C1IE = 1;
     
-    
-    TRISA = 0xFFFF;
-    TRISB = 0xFFFF;
     i = 0;
     while(1){
 //        while(i < 100){++i;}
